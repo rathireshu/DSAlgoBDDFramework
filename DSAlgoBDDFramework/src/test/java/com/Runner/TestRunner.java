4e3153ee0fb38f4329a7d6ecda4dca660134e208
rathireshu <rathi.reshu@gmail.com>
2024-02-12 14:54:15 -0500

    Update TestRunner.java

--- DSAlgoBDDFramework/src/test/java/com/Runner/TestRunner.java
+++ DSAlgoBDDFramework/src/test/java/com/Runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags="",
-		         features = "src/test/resources/Features/4_Graph.feature", 
+		         features = "src/test/resources/Features", 
                  glue = {"com.StepDefinition", "com.ApplicationHooks" }, 
                  monochrome = true, 
 		         dryRun = false, 
