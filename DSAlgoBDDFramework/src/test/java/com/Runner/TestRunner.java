4eb6618cfff9ba592c6db55967fb77d04cd6c811
rathireshu <rathi.reshu@gmail.com>
2024-02-16 17:27:38 -0500

    testjob

--- DSAlgoBDDFramework/src/test/java/com/Runner/TestRunner.java
+++ DSAlgoBDDFramework/src/test/java/com/Runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags="",
-		         features = "src/test/resources/Features/", 
+		         features = "src/test/resources/Features/1_Home.feature", 
                  glue = {"com.StepDefinition", "com.ApplicationHooks" }, 
                  monochrome = true, 
 		         dryRun = false, 
